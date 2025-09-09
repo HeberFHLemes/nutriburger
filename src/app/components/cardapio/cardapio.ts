534cf2c22674463b4b9d567ef19eb90e4f49dece
HeberFHLemes <hfhlemes@gmail.com>
2025-09-09 11:28:17 -0300

    Squashed 'nutriburger-frontend/' content from commit baec878
    git-subtree-dir: nutriburger-frontend
    git-subtree-split: baec878fa9d477f158adc3d81e341e821fa6097b

---
+++ src/app/components/cardapio/cardapio.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CurrencyPipe } from '@angular/common';
+import { KeyValuePipe } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
+
+interface Produto {
+  id: number;
+  nome: string;
+  preco: number;
+  dadosNutricionais: { [key: string]: string | number };
+  ingredientes: string[];
+}
+
+@Component({
+  selector: 'app-cardapio',
+  standalone: true,
+  imports: [CurrencyPipe, KeyValuePipe],
+  templateUrl: './cardapio.html',
+  styleUrl: './cardapio.css'
+})
+
+export class Cardapio implements OnInit{
+  produtos: Produto[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getCardapio();
+  }
+
+  getCardapio(){
+    this.http.get<Produto[]>('http://localhost:8080/cardapio')
+      .subscribe({
+        next: (data) => (this.produtos = data),
+        error: () => { 
+          alert('Erro ao carregar o cardápio...');
+        }
+      });
+  }
+}
